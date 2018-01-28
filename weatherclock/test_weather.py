2770614887b72760584ed7f1b9396991a535bec3
Cynthia Taylor <cynthia.taylor@cantire.com>
2018-01-27 20:09:09 -0500

    Started to add logic to sort forecasts into buckets depending on current time and time of forecast. Added tests

---
+++ weatherclock/test_weather.py
@@ -0,0 +1,48 @@
+import pytest
+import datetime
+import weatherclock
+
+def test_11oclockpm_before_midnight():
+    d = datetime.date(2018, 1, 14)
+    t = datetime.time(23, 00)
+    assert not weatherclock.is_after_midnight(datetime.datetime.combine(d,t))
+
+def test_7oclockam_after_midnight():
+    d = datetime.date(2018, 1, 14)
+    t = datetime.time(7, 00)
+    assert weatherclock.is_after_midnight(datetime.datetime.combine(d,t))
+
+def test_12oclockpm_before_midnight():
+    d = datetime.date(2018, 1, 14)
+    t = datetime.time(12, 00)
+    assert not weatherclock.is_after_midnight(datetime.datetime.combine(d,t))
+
+def test_is_tonight_currently_before_midnight_forecast_10pm():
+    d = datetime.datetime.now().date()
+    forecast_time = datetime.datetime.combine(d, datetime.time(22, 00))
+    sunset_time = datetime.datetime.combine(d, datetime.time(18, 00))
+    sunrise_time = datetime.datetime.combine(d, datetime.time(6, 00))
+    assert weatherclock.is_tonight(forecast_time,sunrise_time,sunset_time,False)
+
+def test_is_tonight_currently_after_midnight_forecast_3am():
+    d = datetime.datetime.now().date()
+    forecast_time = datetime.datetime.combine(d, datetime.time(3, 00))
+    sunset_time = datetime.datetime.combine(d, datetime.time(18, 00))
+    sunrise_time = datetime.datetime.combine(d, datetime.time(6, 00))
+    assert weatherclock.is_tonight(forecast_time,sunrise_time,sunset_time,True)
+
+def test_is_tonight_currently_before_midnight_forecast_1am_tomorrow():
+    d = datetime.datetime.now().date()
+    tomorrowdate = d + datetime.timedelta(days=1)
+    forecast_time = datetime.datetime.combine(tomorrowdate, datetime.time(1, 00))
+    sunset_time = datetime.datetime.combine(d, datetime.time(18, 00))
+    sunrise_time = datetime.datetime.combine(d, datetime.time(6, 00))
+    assert weatherclock.is_tonight(forecast_time,sunrise_time,sunset_time,False)
+
+def test_is_tonight_currently_before_midnight_forecast_11am_tomorrow():
+    d = datetime.datetime.now().date()
+    tomorrowdate = d + datetime.timedelta(days=1)
+    forecast_time = datetime.datetime.combine(tomorrowdate, datetime.time(11, 00))
+    sunset_time = datetime.datetime.combine(d, datetime.time(18, 00))
+    sunrise_time = datetime.datetime.combine(d, datetime.time(6, 00))
+    assert not weatherclock.is_tonight(forecast_time,sunrise_time,sunset_time,False)
--- weatherclock/weatherclock.py
+++ weatherclock/weatherclock.py
@@ -13,17 +13,17 @@ payload={'id':cfg.owm['cityid'],'appid':cfg.owm['apikey'],'units': 'metric'}
 r=requests.get(cfg.owm['url']+'weather',params=payload)
 #now = json.loads(r.text)
 now = r.json()
-
-print('Weather in',now['name'],now['weather'][0]['main'],now['main']['temp'],datetime.datetime.fromtimestamp(int(now['dt'])),'Sunrise: ',datetime.datetime.fromtimestamp(int(now['sys']['sunrise'])).strftime('%I:%M %p'),'Sunset: ',datetime.datetime.fromtimestamp(int(now['sys']['sunset'])).strftime('%I:%M %p'))
-
-payload['cnt'] = 12
-r = requests.get(cfg.owm['url']+'forecast',params=payload)
-forecast = r.json()
-
-print(forecast['cnt'])
+today = {}
+tonight = {}
+tomorrow = {}
 
 def get_time(weather):
     return datetime.datetime.fromtimestamp(int(weather['dt']))
+
+def get_sunrise(weather):
+    return datetime.datetime.fromtimestamp(int(now['sys']['sunrise']))
+def get_sunset(weather):
+    return datetime.datetime.fromtimestamp(int(now['sys']['sunset']))
 
 def get_temp(weather):
     return weather['main']['temp']
@@ -34,8 +34,74 @@ def get_condition(weather):
 def get_rainfall(weather):
     return weather['rain'].get('3h',0) if 'rain' in weather else '0'
 
-for counter,weather in enumerate(forecast['list']):
-    print('...',counter,get_time(weather),
-        'temp:',get_temp(weather),
-        'condition: ',get_condition(weather),
-        'rainfall:', get_rainfall(weather)) 
+def get_snowfall(weather):
+    return weather['snow'].get('3h',0) if 'snow' in weather else '0'
+
+def is_after_midnight(somedate):
+    current_time = somedate.time()
+    if  current_time < datetime.time(12,00):
+        return True
+    else:
+        return False
+
+def is_tonight(forecast_time, sunrise_time, sunset_time,after_midnight):
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+    # print('Passed forecast: ',forecast_time,'Today is ',today)
+    # print('forecast_time.date() == today',forecast_time.date() == today)
+    # print('After sunset? ',forecast_time.time() > sunset_time.time())
+    #1. It is currently after midnight and Forecast time is today before sunrise
+    if (forecast_time.date() == today and after_midnight and forecast_time.time() < sunrise_time.time()):
+        return True
+    #2. It is currently before midnight and Forecast time is today after sunset
+    if (forecast_time.date() == today and not after_midnight and forecast_time.time() > sunset_time.time()):
+        return True
+    #3. It is currently before midnight and Forecast time is tomorrow before sunrise
+    if (forecast_time.date() == tomorrow and not after_midnight and forecast_time.time() < sunrise_time.time()):
+        return True
+    
+    return False
+
+def when_is_it(now,weather):
+    # Today: if forecast is today and forecast time < now sunset
+    # Tonight: (It is currently before midnight and Forecast time is today after sunset or tomorrow before sunrise) or 
+    #           (it is currently after midnight and forecast time is today before sunrise)
+    # Tomorrow: (It is currently before sunrise and forecast time is today) or 
+    #           (it is after sunrise and  Forecast time is tomorrow before sunset)
+    now_datetime = get_time(now)
+    sunrise = get_sunrise(now)
+    sunset = get_sunset(now)
+    forecast_time = get_time(weather)
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+
+    # print('Now: ',now_datetime)
+    # print('Sunset: ',sunset)
+    # print('Tomorrow: ',tomorrow)
+    if forecast_time.date() == today and forecast_time.time() > sunrise.time() and forecast_time.time() < sunset.time():
+        return 'Today'
+    # print('forecast_time.date() == today',forecast_time.date() == today)
+    # print('is_after_midnight(datetime.datetime.now()',is_after_midnight(datetime.datetime.now()))
+    if is_tonight(forecast_time,sunrise,sunset,is_after_midnight(datetime.datetime.now())):
+       return 'Tonight'
+
+    if (forecast_time.date() == tomorrow and forecast_time > sunrise):
+        return 'Tomorrow'
+    else:
+        return 'Not Today or Tomorrow'
+        
+
+if __name__ == '__main__':
+    print('Weather in',now['name'],now['weather'][0]['main'],now['main']['temp'],datetime.datetime.fromtimestamp(int(now['dt'])),'Sunrise: ',datetime.datetime.fromtimestamp(int(now['sys']['sunrise'])).strftime('%I:%M %p'),'Sunset: ',datetime.datetime.fromtimestamp(int(now['sys']['sunset'])).strftime('%I:%M %p'))
+
+    payload['cnt'] = 12
+    r = requests.get(cfg.owm['url']+'forecast',params=payload)
+    forecast = r.json()
+
+    for counter,weather in enumerate(forecast['list']):
+        print('...',counter,get_time(weather),
+            'temp:',get_temp(weather),
+            'condition: ',get_condition(weather),
+            'rainfall:', get_rainfall(weather),
+            'snowfall:', get_snowfall(weather),
+            when_is_it(now,weather)) 
