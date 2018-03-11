25b53be9eeb388db25694ede3bd6bce769884d1b
Cyn42 <cyntaylor42@gmail.com>
2018-03-11 14:20:22 -0400

    Added gamma correction

--- weatherclock/arduino_clockclient/src/main.cpp
+++ weatherclock/arduino_clockclient/src/main.cpp
@@ -13,20 +13,28 @@
 const char* ssid = WIFISSID;
 const char* password = WIFIPASSWORD;
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel grid = Adafruit_NeoPixel(100, PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel connectivity_indicator = Adafruit_NeoPixel(2, WIFIPIN, NEO_GRB + NEO_KHZ800);
+extern const uint8_t gamma8[];
 
 ESP8266WebServer server(80);
 long time = 0;
 long lastCheckin = millis();
+
+void setPixelColor(uint16_t pixel, long red, long green, long blue ) {
+  Serial.println("Gamma correction");
+  uint8_t r = pgm_read_byte(&gamma8[red]);
+  uint8_t g = pgm_read_byte(&gamma8[green]);
+  uint8_t b = pgm_read_byte(&gamma8[blue]);
+  Serial.print(r);
+  Serial.print(", ");
+  Serial.print(g);
+  Serial.print(", ");
+  Serial.print(b);
+  Serial.println("");
+  grid.setPixelColor(pixel,grid.Color(r,g,b));
+  grid.show();
+}
 
 void showOK() {
   connectivity_indicator.setPixelColor(0,connectivity_indicator.Color(0,255,0));
@@ -70,8 +78,8 @@ void show_icon(uint16_t pixels[], int numPixels) {
 
    for (int i=0;i<numPixels;i++) {
      uint16_t pix = pixels[i];
-     grid.setPixelColor(pix, grid.Color(red_value, green_value, blue_value));
-     grid.show();
+     setPixelColor(pix,red_value, green_value, blue_value);
+     //grid.show();
    }
    delay(100);
 
@@ -100,10 +108,10 @@ void set_color(long r, long g, long b) {
   Serial.print(b);
   Serial.println(".");
 
-  grid.setPixelColor(22, grid.Color(r,g,b));
-  grid.setPixelColor(23, grid.Color(r,g,b));
-  grid.setPixelColor(36, grid.Color(r,g,b));
-  grid.setPixelColor(37, grid.Color(r,g,b));
+  setPixelColor(22,r,g,b);
+  setPixelColor(23,r,g,b);
+  setPixelColor(36,r,g,b);
+  setPixelColor(37,r,g,b);
   grid.show();
 
   showOK();
@@ -198,3 +206,21 @@ void loop() {
   }
 
 }
+
+const uint8_t PROGMEM gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
